a097ae4f2d10e47a1370e92d43fa73b75fc9a9ac
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-08 13:27:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,33 @@
 
 console.log("Data Request...");
 
-setTimeout(() => {
-  console.log("Getting ready...");
-
-  const product = {
-    name: 'TV',
-    price: 1000
-  };
-
+const req = new Promise(function(resolve, reject){
   setTimeout(() => {
-    product.status = "ordered";
-    console.log(product);
-  }, 3000);
-}, 2000);
+    console.log("Getting ready...");
+  
+    const product = {
+      name: 'TV',
+      price: 1000
+    };
+  
+    resolve(product);
+
+  }, 2000);
+});
+
+req.then((product) => {
+  const req2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+     product.status = "ordered";
+
+     resolve(product);
+    }, 3000);
+  });
+
+    req2.then((data) => {
+      console.log(data);
+    });
+
+});
+
+
